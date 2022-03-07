a7c4158e1b3dd11367674df06da55b672dceb71d
Suirabu <suirabu.dev@gmail.com>
2022-03-06 19:51:19 -0800

    Add basic command line argument parsing [Quick fix]
    Added basic command line argument parser to get us started. This will
    need to replace with a proper parser down the line, however this is not
    a great concern as this one will serve us well for the time being. :^)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, Smol-Emu!\n");
+#include "error.h"
+
+void print_usage(FILE* stream) {
+    fprintf(stream,
+        "SmolEmu - Smol emulator\n\n"
+        "Usage:\n"
+        "    ./smol-emu <ROM path>\n"
+    );
+}
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        print_usage(stderr);
+        REPORT_ERROR("Invalid usage\n");
+        return 1;
+    }
+
+    // TODO: Implement proper argument parser
+    const char* executable_path = argv[0];
+    const char* rom_path = argv[1];
+
     return 0;
 }
